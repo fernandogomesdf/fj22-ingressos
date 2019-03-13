35fd1053cf5eceaa561d129b2c11a26cd3d32b15
Your Name <you@example.com>
2019-03-13 10:47:43 -0300

    implementacao

--- src/test/java/br/com/caelum/ingresso/validation/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validation/GerenciadorDeSessaoTest.java
@@ -2,13 +2,16 @@ package br.com.caelum.ingresso.validation;
 
 import java.math.BigDecimal;
 import java.time.Duration;
+import java.time.LocalTime;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import br.com.caelum.ingresso.gerenciador.GerenciadorDeSessao;
 import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
 public class GerenciadorDeSessaoTest {
@@ -19,6 +22,13 @@ public class GerenciadorDeSessaoTest {
 		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
 		
 		Filme filme = new Filme("Rogue One",Duration.ofMinutes(120),"SCI-FI", BigDecimal.ONE);
+		LocalTime horario = LocalTime.parse("10:00:00");
+		Sala sala = new Sala("");
+		
+		Sessao sessao = new Sessao(horario, sala, filme);
+		boolean cabe = gerenciador.cabe(sessao);
+		
+		Assert.assertTrue(cabe);
 	}
 
 }
