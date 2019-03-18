eb80f5e83e9729a6c07188ddd7211c5dfcdd76ed
Your Name <you@example.com>
2019-03-18 11:52:55 -0300

    implementacao detalhe

--- src/main/java/br/com/caelum/ingresso/client/OmdbClient.java
+++ src/main/java/br/com/caelum/ingresso/client/OmdbClient.java
@@ -5,19 +5,17 @@ import java.util.Optional;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 
 @Component
 public class OmdbClient {
 	
-	public Optional<DetalhesDoFilme> request(Filme filme) {
+	public <T> Optional<T> request(Filme filme, Class<T> tClass) {
 		try {
 			RestTemplate client = new RestTemplate();
 			String titulo = filme.getNome().replaceAll(" ", "+");
 			String url = String.format("https://omdb-fj22.herokuapp.com/movie?title=%s",  titulo);
-			DetalhesDoFilme detalhesDoFilme = client.getForObject(url, DetalhesDoFilme.class);
-			return Optional.ofNullable(detalhesDoFilme);
+			return Optional.of(client.getForObject(url, tClass));
 		} catch (Exception e) {
 			return Optional.empty();
 		}
--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -8,13 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.caelum.ingresso.client.OmdbClient;
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
 
@@ -29,6 +32,9 @@ public class SessaoController {
 	
 	@Autowired
 	private SessaoDao sessaoDao;
+	
+	@Autowired
+	private OmdbClient client;
 	
 	@GetMapping({"/admin/sessao"})
     public ModelAndView form(@RequestParam("salaId")Optional<Integer> salaId, SessaoForm form){
@@ -52,4 +58,16 @@ public class SessaoController {
         
         return new ModelAndView("redirect:/admin/sala/"+form.getSalaId()+"/sessoes");
     }
+	
+	@PostMapping({"/sessao/{id}/lugares"})
+	public ModelAndView lugaresNaSessao(@PathVariable("id") Integer sessaoId){
+		 ModelAndView modelAndView = new ModelAndView("sessao/lugares");
+		 
+		 Sessao sessao = sessaoDao.findOne(sessaoId);
+		 Optional<ImagemCapa> imagemCapa = client.request(sessao.getFilme(), ImagemCapa.class);
+		 modelAndView.addObject("sessao",sessao);
+		 modelAndView.addObject("imagemCapa",imagemCapa.orElse(new ImagemCapa()));
+		 
+		 return modelAndView;
+	}
 }
